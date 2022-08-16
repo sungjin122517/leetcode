e68517a96d24124a7653c32432c5437f680f63b9
sungjin122517 <sungjin122517@naver.com>
2022-08-16 20:37:30 +0900

    (leetcode) [linked list] merge two sorted lists

--- linked-list/merge-two-sorted-lists.cpp
+++ linked-list/merge-two-sorted-lists.cpp
@@ -95,3 +95,24 @@ ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
     return (dummy.next);
 }
 
+
+// Recursion
+ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+    ListNode* result = nullptr;
+
+    if (list1 == nullptr)
+        return list2;
+    else if (list2 == nullptr)
+        return list1;
+
+    if (list1->val <= list2->val) {
+        result = list1;
+        result->next = mergeTwoLists(list1->next, list2);
+    }
+    else {
+        result = list2;
+        result->next = mergeTwoLists(list1, list2->next);
+    }
+
+    return result;
+}
