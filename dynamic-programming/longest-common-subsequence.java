4d276611f49ba47f622a338f0209e677f1742b4c
sungjin122517 <sungjin122517@naver.com>
2023-01-07 23:38:40 +0900

    leetcode [dp] longest common subsequence

---
+++ dynamic-programming/longest-common-subsequence.java
@@ -0,0 +1,29 @@
+class Solution {
+
+    public int longestCommonSubsequence(String text1, String text2) {
+        int[][] arr = new int[text1.length()+1][text2.length()+1];
+        // System.out.print(arr[0][0]);
+
+        for (int i=1; i<=text1.length(); i++) {
+            for (int j=1; j<=text2.length(); j++) {
+                // if (i==0 || j==0) { continue; }
+                if (text1.charAt(i-1) == text2.charAt(j-1) ) {
+                    arr[i][j] = arr[i-1][j-1] + 1;
+                }
+                else if (arr[i-1][j] >= arr[i][j-1]) {
+                    arr[i][j] = arr[i-1][j];
+                }
+                else {
+                    arr[i][j] = arr[i][j-1];
+                }
+            }
+        }
+        return arr[text1.length()][text2.length()];
+    }
+
+    // public static void main(String[] args){
+    //     // some code
+    //     System.out.print(ans);
+    // }
+}
+
