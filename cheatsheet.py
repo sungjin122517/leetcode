aaee80b08461c89ae0aa610dae887ec877951312
sungjin122517 <sungjin122517@naver.com>
2024-04-18 01:43:22 +0800

    update minor typo error

--- cheatsheet.py
+++ cheatsheet.py
@@ -6,10 +6,10 @@ queue = deque()
 # 직접 input 받을 때: list, map 사용
 
 # 공백을 기준으로 구분된 데이터를 입력 받을 때
-data = list(map(int, input.split()))
+data = list(map(int, input().split()))
 
 # 공백을 기준으로 구분된 데이터가 많지 않다면
-a, b, c = map(int, input.split())
+a, b, c = map(int, input().split())
 
 # sys stdin을 활용해 입출력
 import sys
